b4d3fddd1358838dc49a7b57de21b02b2861c595
alomj <vladvoytkiv321@gmail.com>
2024-05-25 21:53:13 +0300

    updated

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from game import Game
 from board import Board
-size = (9,9)
 screen_width = 800
 screen_height = 800
 screen_size = (screen_width, screen_height)
-board = Board(size)
+size = (9,9)
+probability = 0.5
+board = Board(size, probability)
 game = Game(board, screen_size)
 game.run()
