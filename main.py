807a15f1e2cb37219a57c5d7e7b22fba7c722760
alomj <vladvoytkiv321@gmail.com>
2024-05-25 20:38:52 +0300

    changed size for board

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from game import Game
-board = None
+from board import Board
+size = (9,9)
 screen_width = 800
 screen_height = 800
 screen_size = (screen_width, screen_height)
+board = Board(size)
 game = Game(board, screen_size)
 game.run()
