91d8b759445b384bd496d7a7c33a6dfcee278d67
alomj <vladvoytkiv321@gmail.com>
2024-05-25 21:42:14 +0300

    added "probability"

--- board.py
+++ board.py
@@ -1,17 +1,21 @@
+from piece import Piece
+from random import random
 class Board():
-    def __init__(self, size):
+    def __init__(self, size, probability):
         self.size = size
+        self.probability = probability
         self.set_board()
 
 
     def set_board(self):
         self.board = []
         for row in range(self.size[0]):
-            row = []
+            row_pieces = []
             for col in range(self.size[1]):
-                piece = None
-                row.append(piece)
-            self.board.append(row)
+                has_bomb = random() < self.probability
+                piece = Piece(has_bomb)
+                row_pieces.append(piece)
+            self.board.append(row_pieces)
 
     def get_size(self):
         return self.size
