cbfad8d059f82f3f513501de6b88978775279be3
alomj <vladvoytkiv321@gmail.com>
2024-05-26 23:46:08 +0300

    Some refactoring according to PEP 8 conventions

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 from piece import Piece
 from random import random
 
-class Board():
+class Board:
     def __init__(self, size, probability):
         self.size = size
         self.probability = probability
@@ -11,14 +11,13 @@ class Board():
         self.num_non_bombs = 0
         self.set_board()
 
-
     def set_board(self):
         self.board = []
         for row in range(self.size[0]):
             row_pieces = []
             for col in range(self.size[1]):
                 has_bomb = random() < self.probability
-                if (not has_bomb):
+                if not has_bomb:
                     self.num_non_bombs += 1
                 piece = Piece(has_bomb)
                 row_pieces.append(piece)
@@ -36,7 +35,9 @@ class Board():
         neighbors = []
         for row in range(index[0] - 1, index[0] + 2):
             for col in range(index[1] - 1, index[1] + 2):
-                out_of_bounds = row < 0 or row >= self.size[0] or col < 0 or col >= self.size[1]
+                out_of_bounds = (
+                    row < 0 or row >= self.size[0] or col < 0 or col >= self.size[1]
+                )
                 same = row == index[0] and col == index[1]
                 if same or out_of_bounds:
                     continue
@@ -50,24 +51,24 @@ class Board():
         return self.board[row][col]
 
     def handle_click(self, piece, flag):
-        if (piece.get_clicked() or (not flag and piece.get_flagged())):
+        if piece.get_clicked() or (not flag and piece.get_flagged()):
             return
-        if (flag):
+        if flag:
             piece.toggle_flag()
             return
         piece.click()
-        if  (piece.get_has_bomb()):
+        if piece.get_has_bomb():
             self.lost = True
             return
         self.num_clicked += 1
-        if (piece.get_num_around() != 0):
+        if piece.get_num_around() != 0:
             return
         for neighbor in piece.get_neighbors():
-            if (not neighbor.get_has_bomb() and not neighbor.get_clicked()):
+            if not neighbor.get_has_bomb() and not neighbor.get_clicked():
                 self.handle_click(neighbor, False)
-
 
     def get_lost(self):
         return self.lost
+
     def get_won(self):
         return self.num_non_bombs == self.num_clicked
