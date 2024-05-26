8105a94b6fdff80952e2564b358359058d355508
alomj <vladvoytkiv321@gmail.com>
2024-05-26 23:47:47 +0300

    Some refactoring according to PEP 8 conventions

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
 import pygame
 import os
 
-class Game():
+class Game:
     def __init__(self, board, screen_size):
         self.board = board
         self.screen_size = screen_size
-        self.piece_size = self.screen_size[0] // self.board.get_size()[1], self.screen_size[1] // self.board.get_size()[0]
+        self.piece_size = (
+            self.screen_size[0] // self.board.get_size()[1],
+            self.screen_size[1] // self.board.get_size()[0]
+        )
         self.load_images()
 
     def run(self):
@@ -37,14 +40,14 @@ class Game():
         pygame.quit()
 
     def draw(self):
-        topLeft = (0, 0)
+        top_left = (0, 0)
         for row in range(self.board.get_size()[0]):
             for col in range(self.board.get_size()[1]):
                 piece = self.board.get_piece(row, col)
                 image = self.get_image(piece)
-                self.screen.blit(image, topLeft)
-                topLeft = (topLeft[0] + self.piece_size[0], topLeft[1])
-            topLeft = (0, topLeft[1] + self.piece_size[1])
+                self.screen.blit(image, top_left)
+                top_left = (top_left[0] + self.piece_size[0], top_left[1])
+            top_left = (0, top_left[1] + self.piece_size[1])
 
     def load_images(self):
         self.images = {}
@@ -56,25 +59,24 @@ class Game():
             self.images[filename.split(".")[0]] = image
 
     def get_image(self, piece):
-        string = None
         if piece.get_clicked():
-            string = "bomb-at-clicked-block" if piece.get_has_bomb() else str(piece.get_num_around())
-        else:
-            string = "flag" if piece.get_flagged() else "empty-block"
-
-        return self.images[string]
+            if piece.get_has_bomb():
+                return self.images["bomb-at-clicked-block"]
+            return self.images[str(piece.get_num_around())]
+        if piece.get_flagged():
+            return self.images["flag"]
+        return self.images["empty-block"]
 
     def handle_click(self, position, right_click):
-        if (self.board.get_lost()) or self.board.get_won():
+        if self.board.get_lost() or self.board.get_won():
             return
         row, col = position[1] // self.piece_size[1], position[0] // self.piece_size[0]
         piece = self.board.get_piece(row, col)
         self.board.handle_click(piece, right_click)
 
-
     def display_message(self, message, color):
         pygame.font.init()
-        self.font = pygame.font.Font('freesansbold.ttf', 74)
-        text = self.font.render(message, True, color)
+        font = pygame.font.Font('freesansbold.ttf', 74)
+        text = font.render(message, True, color)
         text_rect = text.get_rect(center=(self.screen_size[0] // 2, self.screen_size[1] // 2))
         self.screen.blit(text, text_rect)
