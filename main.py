b5f08b48c032dae8db88c703a7d40c0f0ee7716f
alomj <vladvoytkiv321@gmail.com>
2024-05-26 23:37:06 +0300

    Some refactoring according to PEP 8 conventions

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from game import Game
 from board import Board
-screen_width = 800
-screen_height = 800
-screen_size = (screen_width, screen_height)
-size = (9, 9)
-probability = 0.5
-board = Board(size, probability)
-game = Game(board, screen_size)
-game.run()
+
+
+def main():
+    screen_width = 800
+    screen_height = 800
+    screen_size = (screen_width, screen_height)
+    size = (9, 9)
+    probability = 0.5
+    board = Board(size, probability)
+    game = Game(board, screen_size)
+    game.run()
+
+
+if __name__ == '__main__':
+    main()
